d500d0563acfa0becb293247d87c2c5afccc9b48
Daniel alias Comment God <31951102+dani832m@users.noreply.github.com>
2017-11-07 09:15:35 +0100

    Kommentarer tilrettet

--- src/dk/n4/Opg12_14.java
+++ src/dk/n4/Opg12_14.java
@@ -12,25 +12,26 @@ public class Opg12_14 {
 
     public static void main(String[] args) throws Exception { //Bemærk: throws Exception
 
-        // Laver et nyt objekt i Scanner-klassen med navnet "input"
+        //Laver et nyt objekt i Scanner-klassen med variabelnavnet "input"
         Scanner input = new Scanner(System.in);
 
-        // Giver brugeren mulighed for at indtaste en sti til score-filen
+        //Giver brugeren mulighed for at indtaste en sti til score-filen
         System.out.print("Indtast navnet på filen: ");
+        
         //Laver et nyt objekt i File-klassen med navnet "file"
         File file = new File(input.nextLine()); //Fortæller at "file" skal baseres på bruger-input
 
-        // Tjekker om objektet eksisterer, hvis ikke lukkes systemet
+        //Tjekker om objektet eksisterer - hvis ikke, så lukkes systemet
         if (!file.exists()) {
             System.out.println("Filen '" + file + "' eksisterer ikke.");
-            System.exit(1);
+            System.exit(1); //Systemet lukkes
         }
 
         int count = 0; //Deklarerer og initialiserer variabel "count" - bruges ved resultaterne
         double samlet = 0; //Deklarerer og initialiserer variabel "samlet" - bruges ved resultaterne
 
-        try ( //Try der automatisk lukker filen efter brug
-                //Laver objekt i Scanner-klassen og initialiserer den til at være brugerens fil
+        try ( //Try-block der automatisk lukker filen efter brug
+                //Laver objekt i Scanner-klassen, og initialiserer den til at være brugerens fil
                 Scanner inputFile = new Scanner(file);
         ) {
             while (inputFile.hasNext()) { //Kører while-loop baseret på indhold af brugerens fil
