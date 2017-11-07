664663256302990eca6fc89e86ebf255d033a0d7
Daniel alias Comment God <31951102+dani832m@users.noreply.github.com>
2017-11-07 09:53:51 +0100

    Kommentarer tilrettet

--- src/dk/n4/Opg12_3.java
+++ src/dk/n4/Opg12_3.java
@@ -10,12 +10,12 @@ import java.util.Scanner; //Importerer klassen "Scanner"
 
 public class Opg12_3 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Main-metode eksekveres
 
-        double ran = Math.random() * 100; //Anvender double fordi "Math.random" som int altid vil give 0
+        double ran = Math.random() * 100; //Anvender "double" fordi "Math.random" som "int" altid vil give 0
         int[] array = new int[100]; //Nyt array, der kan indeholde 100 int-værdier
 
-        for(int i = 0; i < array.length; i++) { //for-loop der kører, hvis variablen "i" er mindre end index-længde
+        for(int i = 0; i < array.length; i++) { //For-loop der kører, hvis variablen "i" er mindre end index-længde
             array[i] = (int)ran; //Caster "ran" til "int" for ikke at få kommatal, og initialiserer værdien i arrays index
             ran = Math.random() * 100; //Tildeler variablen "ran" en ny tilfældig værdi, og loopet køres igen
         }
@@ -25,10 +25,11 @@ public class Opg12_3 {
         Scanner input = new Scanner (System.in); //Laver nyt scanner-objekt
         System.out.println("Indtast et index, du vil se værdien på: ");
 
-        try {
+        try { //Try-block
             int index = input.nextInt(); //Deklarerer "index" og initialiseres til at være brugerinput
             System.out.println("Array[" + index + "] = " + array[index]); //Printer valgte index og værdi af dette
         }
+        //To exceptions integreres i catch-blokke
         catch (IndexOutOfBoundsException ex) { //Hvis valgte index ikke er mellem 1-100 bliver det catched
             System.out.println("Out of Bounds."); //Og besked printes
         }
@@ -36,6 +37,6 @@ public class Opg12_3 {
             System.out.println("Ugyldig indtastning!"); //Printes hvis der ikke anvendes legal input (integers)
         }
 
-    }
+    } //Main afsluttes
 
-}
+} //Klassen afsluttes
