98ec7981cd3e2851bc2b4938d092a6271e5a98fd
Daniel alias Comment God <31951102+dani832m@users.noreply.github.com>
2017-11-07 09:21:05 +0100

    Kommentarer tilrettet

--- src/dk/n4/Opg12_15.java
+++ src/dk/n4/Opg12_15.java
@@ -11,22 +11,22 @@ import java.util.Scanner; //Importerer klaassen "Scanner"
 
 public class Opg12_15 {
 
-    public static void main(String[] args) throws Exception { //Main eksekveres
+    public static void main(String[] args) throws Exception { //Main eksekveres - bemærk: throws Exception
 
-        //SEKTION 1: Vi initialiserer et objekt (kaldet "file") i File-klassen til at en .txt-fil
+        //SEKTION 1: Vi initialiserer et objekt (kaldet "file") i File-klassen til at være en .txt-fil
 
         java.io.File file = new java.io.File("Exercise12_15.txt");
 
         //Hvis det initialiserede objekt allerede eksisterer, så lukkes systemet, og resten af koden eksekveres ikke!
         if (file.exists()) {
             System.out.println("Filen eksisterer allerede!");
-            System.exit(0);
+            System.exit(0); //Systemet lukkes
         }
 
         //SEKTION 2: Vi åbner filen, skriver til filen, og lukker filen igen
 
         try ( //Auto-closer filen, når vi er færdige med at indsætte data i den
-              // Laver nyt PrintWriter objekt og initialiserer det til at være vores file-objekt
+              // Laver nyt PrintWriter-objekt og initialiserer det til at være vores file-objekt
               java.io.PrintWriter output = new java.io.PrintWriter(file) //Nu kan vi skrive til filen
         ) {
             // Skriver 120 tilfældige int-værdier i filen vha. Math.random
@@ -39,10 +39,10 @@ public class Opg12_15 {
 
         //SEKTION 3: Vi åbner filen igen, og læser filens indhold
 
-        // Laver nyt scanner-objekt og initialiserer det med vores file-objekt
+        //Laver nyt scanner-objekt og initialiserer det med vores file-objekt
         Scanner input = new Scanner(file); //Nu kan vi læse fra filen
 
-        // Læser data fra fil vha. while-loop
+        //Læser data fra fil vha. while-loop
         while (input.hasNext()) {
             int value = input.nextInt();
             System.out.print(value + " "); //Printer data i konsollen
@@ -50,6 +50,6 @@ public class Opg12_15 {
 
         input.close(); //Vi lukker filen igen, efter vi har indlæst
 
-    }
+    } //Main() afsluttes
 
-}
+} //Klassen afsluttes
